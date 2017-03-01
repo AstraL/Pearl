a35b155c1c32b480c9990e1efae084943090ad49
AstraL <sawyer126@gmail.com>
2017-03-01 02:09:04 +0200

    add user to migration

--- db/migrate/20170204171559_devise_create_users.rb
+++ db/migrate/20170204171559_devise_create_users.rb
@@ -42,5 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+    u = User.create(login: "astral", email: "sawyer126@gmail.com", password: "dh390j239j", password_confirmation: "dh390j239j")
+    u.save
   end
 end
