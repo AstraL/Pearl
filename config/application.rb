1e6a4319f2fb65ed007e128f16adf7cc2fb778a5
AstraL <sawyer126@gmail.com>
2017-08-15 03:52:40 +0300

    complex bg with slogan

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+ENV['RAILS_ADMIN_THEME'] = 'material_theme'
 
 module Mitnitsa
   class Application < Rails::Application
--- db/migrate/20170406005135_add_disabled_to_houses.rb
+++ db/migrate/20170406005135_add_disabled_to_houses.rb
@@ -1,8 +1,5 @@
 class AddDisabledToHouses < ActiveRecord::Migration[5.0]
   def change
     add_column :houses, :disabled, :boolean, default: false
-
-    h_disabled = House.create(title: 'Друга черга', address: 'вул. Козацька 1/2', disabled: true)
-    h_disabled.save
   end
 end
